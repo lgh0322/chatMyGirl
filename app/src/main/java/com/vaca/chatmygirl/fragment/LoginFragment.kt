3a656427ffd0fd8145f903204db1589686c4173b
李光浩 <liguanghao@viatomtech.com>
2021-09-01 14:44:23 +0800

    good day

---
+++ app/src/main/java/com/vaca/chatmygirl/fragment/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.vaca.chatmygirl.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vaca.chatmygirl.databinding.FragmentLoginBinding
+
+class LoginFragment: Fragment() {
+
+    lateinit var binding:FragmentLoginBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+
+
+        binding= FragmentLoginBinding.inflate(inflater,container,false)
+
+
+        return binding.root
+    }
+}
