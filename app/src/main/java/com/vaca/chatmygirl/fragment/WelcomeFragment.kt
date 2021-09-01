ca63dfbb6611a9aa4a1545c415d199824c93e862
李光浩 <liguanghao@viatomtech.com>
2021-09-01 14:46:29 +0800

    good day rsdgfaer

---
+++ app/src/main/java/com/vaca/chatmygirl/fragment/WelcomeFragment.kt
@@ -0,0 +1,26 @@
+package com.vaca.chatmygirl.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vaca.chatmygirl.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment:Fragment() {
+
+
+
+    lateinit var binding:FragmentWelcomeBinding
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding= FragmentWelcomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
