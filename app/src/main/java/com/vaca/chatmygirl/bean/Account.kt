ba5a9e350a67fd2ccfd4db574b24de877b5361ea
李光浩 <liguanghao@viatomtech.com>
2021-09-01 15:30:04 +0800

    serytedrgsfgdfdsfg

---
+++ app/src/main/java/com/vaca/chatmygirl/bean/Account.kt
@@ -0,0 +1,3 @@
+package com.vaca.chatmygirl.bean
+
+data class Account(val user:String, val Password)
--- app/src/main/java/com/vaca/chatmygirl/data/MyStorage.kt
+++ app/src/main/java/com/vaca/chatmygirl/data/MyStorage.kt
@@ -1,7 +1,16 @@
 package com.vaca.chatmygirl.data
 
+import com.blankj.utilcode.util.SPUtils
+
 object MyStorage {
 
+    val UserName="UserName"
+    val UserPassword="UserPassword"
+
+    fun getAccount(){
+        return SPUtils.getInstance().getString("")
+
+    }
 
 
 
