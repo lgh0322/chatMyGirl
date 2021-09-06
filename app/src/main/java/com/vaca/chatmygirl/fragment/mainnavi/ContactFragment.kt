5977f1bf254b382754f5275a191b10880d8c299d
李光浩 <liguanghao@viatomtech.com>
2021-09-06 15:24:14 +0800

    fbvdfthdrgr风格和

--- app/src/main/java/com/vaca/chatmygirl/fragment/mainnavi/ContactFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/fragment/mainnavi/ContactFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vaca.chatmygirl.adapter.ContactButtonAdapter
+import com.vaca.chatmygirl.adapter.RecordButtonAdapter
 import com.vaca.chatmygirl.databinding.FragmentContactBinding
 import com.vaca.chatmygirl.databinding.FragmentLoginBinding
 import com.vaca.chatmygirl.databinding.FragmentMainBinding
@@ -12,7 +16,9 @@ import com.vaca.chatmygirl.databinding.FragmentMainBinding
 class ContactFragment: Fragment() {
 
     lateinit var binding:FragmentContactBinding
-
+    companion object {
+        val initJump = MutableLiveData<Int>()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +28,15 @@ class ContactFragment: Fragment() {
 
 
         binding= FragmentContactBinding.inflate(inflater,container,false)
-
+        val buttonAdapter =ContactButtonAdapter(requireContext())
+        val lm = object : LinearLayoutManager(requireContext(), HORIZONTAL, false) {
+            override fun canScrollHorizontally(): Boolean {
+                return false
+            }
+        }
+        binding.topButton.layoutManager = lm
+        buttonAdapter.addAll(arrayOf("朋友", "群组"))
+        binding.topButton.adapter=buttonAdapter
 
         return binding.root
     }
