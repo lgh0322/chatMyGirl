e1ace83bba2bc4b4a06eff89ba44636b48fbfcec
liguanghao <liguanghao@viatomtech.com>
2021-10-30 17:09:40 +0800

    65625sdfdfgSDF

---
+++ app/src/main/java/com/vaca/chatmygirl/MainApplication.kt
@@ -0,0 +1,28 @@
+package com.vaca.chatmygirl
+
+import android.app.Application
+
+import com.tencent.bugly.crashreport.CrashReport
+import com.vaca.chatmygirl.utils.PathUtil
+
+
+class MainApplication : Application() {
+
+    companion object {
+        lateinit var application: Application
+    }
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+        PathUtil.initVar(this)
+
+
+        application = this
+        CrashReport.initCrashReport(this, "ab96e0dbf2", false);
+
+    }
+
+
+}
--- app/src/main/java/com/vaca/chatmygirl/net/NetCmd.kt
+++ app/src/main/java/com/vaca/chatmygirl/net/NetCmd.kt
@@ -1,6 +1,12 @@
 package com.vaca.chatmygirl.net
 
+import com.vaca.chatmygirl.BuildConfig
+import io.socket.client.IO
+
 object NetCmd {
 
+
+
+    val mSocket= IO.socket(BuildConfig.HOST)
 
 }
