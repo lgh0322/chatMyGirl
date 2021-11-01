74f9afb3ccb44cc1f6ee93f475ad286118e3a0c8
liguanghao <liguanghao@viatomtech.com>
2021-11-01 16:03:17 +0800

    dfsdfdg

--- app/src/main/java/com/vaca/chatmygirl/net/NetCmd.kt
+++ app/src/main/java/com/vaca/chatmygirl/net/NetCmd.kt
@@ -9,45 +9,51 @@ import org.json.JSONObject
 object NetCmd {
     private val mSocket = IO.socket(BuildConfig.HOST)
 
-    private val PURPOSE="purpose"
+    private val PURPOSE = "purpose"
 
-    fun initSocket(){
+    fun initSocket() {
         mSocket.connect()
-        mSocket.on("loginSuccess",object :Emitter.Listener{
+        mSocket.on("Server", object : Emitter.Listener {
             override fun call(vararg args: Any?) {
-                val fuck=args[0] as Int
-                Log.e("fuck",fuck.toString())
+                val fuck = args[0]
+                Log.e("fuck", fuck.toString())
+            }
+
+        }
+        )
+
+        mSocket.on("Relay", object : Emitter.Listener {
+            override fun call(vararg args: Any?) {
+                val fuck = args[0]
+                Log.e("fuck", fuck.toString())
             }
 
         }
         )
     }
 
-    private fun sendServer(msg:String){
-        mSocket.emit("Server",msg)
+    private fun sendServer(msg: String) {
+        mSocket.emit("Server", msg)
     }
 
-    private fun sendRelay(msg:String){
-        mSocket.emit("Relay",msg)
+    private fun sendRelay(msg: String) {
+        mSocket.emit("Relay", msg)
     }
 
 
-
-
-
-    fun login(user:String,password:String){
-        val msg=JSONObject()
-        msg.put(PURPOSE,"login")
-        msg.put("username",user)
-        msg.put("password",password)
+    fun login(user: String, password: String) {
+        val msg = JSONObject()
+        msg.put(PURPOSE, "login")
+        msg.put("username", user)
+        msg.put("password", password)
         sendServer(msg.toString())
     }
 
-    fun signUp(user:String,password:String){
-        val msg=JSONObject()
-        msg.put(PURPOSE,"signup")
-        msg.put("username",user)
-        msg.put("password",password)
+    fun signUp(user: String, password: String) {
+        val msg = JSONObject()
+        msg.put(PURPOSE, "signup")
+        msg.put("username", user)
+        msg.put("password", password)
         sendServer(msg.toString())
     }
 
