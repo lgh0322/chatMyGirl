c2750e2653559089b80a656caa0587c6b7456163
liguanghao <liguanghao@viatomtech.com>
2021-11-02 15:58:49 +0800

    edfdfdgghtg

--- app/src/main/java/com/vaca/chatmygirl/fragment/SignUpFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/fragment/SignUpFragment.kt
@@ -1,16 +1,45 @@
 package com.vaca.chatmygirl.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.vaca.chatmygirl.R
 import com.vaca.chatmygirl.databinding.FragmentLoginBinding
 import com.vaca.chatmygirl.databinding.FragmentSignUpBinding
 
 class SignUpFragment: Fragment() {
 
     lateinit var binding:FragmentSignUpBinding
+
+    fun isReady():Boolean{
+        if(binding.x1.text.toString().isNullOrEmpty()){
+            return false
+        }
+        if(binding.x2.text.toString().isNullOrEmpty()){
+            return false
+        }
+        return true
+    }
+
+    fun changeBt(){
+        if(isReady()){
+            binding.signUp.apply {
+                background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.login_button)
+
+            }
+        }else{
+            binding.signUp.apply {
+                background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.login_button_before)
+            }
+        }
+    }
 
 
     override fun onCreateView(
@@ -22,6 +51,39 @@ class SignUpFragment: Fragment() {
 
         binding= FragmentSignUpBinding.inflate(inflater,container,false)
 
+        binding.x1.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                changeBt()
+            }
+
+        })
+
+        binding.x2.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                changeBt()
+            }
+
+        })
+
+        binding.signUp.setOnClickListener {
+
+        }
 
         return binding.root
     }
