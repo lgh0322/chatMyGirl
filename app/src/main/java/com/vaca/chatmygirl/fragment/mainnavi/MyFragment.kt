f2a3204dcd48a07175f04e2cd501acd60ef18f84
liguanghao <liguanghao@viatomtech.com>
2021-11-04 15:42:30 +0800

    kjhk4565

--- app/src/main/java/com/vaca/chatmygirl/fragment/mainnavi/MyFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/fragment/mainnavi/MyFragment.kt
@@ -1,6 +1,9 @@
 package com.vaca.chatmygirl.fragment.mainnavi
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,7 @@ import com.vaca.chatmygirl.R
 import com.vaca.chatmygirl.databinding.FragmentLoginBinding
 import com.vaca.chatmygirl.databinding.FragmentMainBinding
 import com.vaca.chatmygirl.databinding.FragmentMyBinding
+import java.lang.Exception
 
 class MyFragment: Fragment() {
 
@@ -27,6 +31,17 @@ class MyFragment: Fragment() {
 
         binding.changeInfo.setOnClickListener {
             findNavController().navigate(R.id.action_myFragment_to_userInfoFragment)
+        }
+
+        binding.rlSetting2.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", requireContext().packageName, null)
+            intent.data = uri
+            try {
+                requireContext().startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
 
         return binding.root
--- app/src/main/java/com/vaca/chatmygirl/fragment/mainnavi/UserInfoFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/fragment/mainnavi/UserInfoFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.vaca.chatmygirl.databinding.FragmentLoginBinding
 import com.vaca.chatmygirl.databinding.FragmentMainBinding
 import com.vaca.chatmygirl.databinding.FragmentMyBinding
@@ -23,6 +24,12 @@ class UserInfoFragment: Fragment() {
 
 
         binding=FragmentUserInfoBinding.inflate(inflater,container,false)
+
+        binding.loginBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+
 
 
         return binding.root
