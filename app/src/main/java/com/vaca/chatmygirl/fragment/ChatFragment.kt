8e6b9398c87aaa75337c8d282a87fd11f7cba84a
liguanghao <liguanghao@viatomtech.com>
2021-11-05 16:35:44 +0800

    优化覆盖吧54456dsf56drtgrgfhjv

--- app/src/main/java/com/vaca/chatmygirl/fragment/ChatFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/fragment/ChatFragment.kt
@@ -1,11 +1,14 @@
 package com.vaca.chatmygirl.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.vaca.chatmygirl.databinding.FragmentChatBinding
+import com.vaca.chatmygirl.net.NetCmd
 import com.vaca.chatmygirl.utils.SoftInputUtil
 
 
@@ -40,12 +43,32 @@ class ChatFragment: Fragment() {
 
         binding= FragmentChatBinding.inflate(inflater,container,false)
         attachView()
-      /*  binding.messageSend.setOnClickListener {
+        binding.messageSend.setOnClickListener {
             val text=binding.chatMessage.text.toString()
             if(text.isNotEmpty()){
                 NetCmd.chatText(text,false)
             }
-        }*/
+        }
+
+        binding.chatMessage.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val text=binding.chatMessage.text.toString()
+                if(text.isEmpty()){
+                    binding.messageSend.visibility=View.GONE
+                }else{
+                    binding.messageSend.visibility=View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
 
         return binding.root
     }
