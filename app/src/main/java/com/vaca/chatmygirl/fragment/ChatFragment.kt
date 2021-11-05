1f1e83225e3aff29cf241222c4c8d3242c16d19c
liguanghao <liguanghao@viatomtech.com>
2021-11-05 16:21:21 +0800

    优化覆盖吧54456456dfg

--- app/src/main/java/com/vaca/chatmygirl/fragment/ChatFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/fragment/ChatFragment.kt
@@ -5,15 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vaca.chatmygirl.databinding.FragmentChat2Binding
 import com.vaca.chatmygirl.databinding.FragmentChatBinding
-import com.vaca.chatmygirl.databinding.FragmentLoginBinding
-import com.vaca.chatmygirl.databinding.FragmentMainBinding
-import com.vaca.chatmygirl.net.NetCmd
 import com.vaca.chatmygirl.utils.SoftInputUtil
-
-import android.R
-import com.vaca.chatmygirl.utils.SoftInputUtil.ISoftInputChanged
 
 
 class ChatFragment: Fragment() {
@@ -24,15 +17,15 @@ class ChatFragment: Fragment() {
     private fun attachView() {
 
         //editText2为需要调整的View
-        val editText2 = binding.constraintLayout
+        val editText = binding.constraintLayout
         val softInputUtil = SoftInputUtil()
         softInputUtil.attachSoftInput(
-            editText2
+            editText
         ) { isSoftInputShow, softInputHeight, viewOffset ->
             if (isSoftInputShow) {
-                editText2.setTranslationY(binding.constraintLayout.getTranslationY() - viewOffset)
+                editText.setTranslationY(binding.constraintLayout.getTranslationY() - viewOffset)
             } else {
-                editText2.setTranslationY(0f)
+                editText.setTranslationY(0f)
             }
         }
     }
