2e3a7ccacc5e483542148e297bcf1b9497ea2165
liguanghao <liguanghao@viatomtech.com>
2021-12-07 16:37:36 +0800

    dfgfg

--- app/src/main/java/com/vaca/chatmygirl/optionfragment/SendOptionFragment.kt
+++ app/src/main/java/com/vaca/chatmygirl/optionfragment/SendOptionFragment.kt
@@ -32,10 +32,7 @@ class SendOptionFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
         binding = FragmentSendOptionBinding.inflate(inflater, container, false)
-
         binding.camera.setOnClickListener {
             //"点击了照相";
             //  6.0之后动态申请权限 摄像头调取权限,SD卡写入权限
@@ -52,16 +49,18 @@ class SendOptionFragment : Fragment() {
                 ActivityCompat.requestPermissions(
                     requireActivity(),
                     arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                 65
+                    65
                 )
                 return@setOnClickListener
             }
             val intent = Intent()
             intent.action = MediaStore.ACTION_IMAGE_CAPTURE
-
-
             val fileJA: File = File(PathUtil.getPathX("fuck.jpg"))
-            val uri = FileProvider.getUriForFile(requireContext(), "com.vaca.chatmygirl.fileProvider", fileJA)
+            val uri = FileProvider.getUriForFile(
+                requireContext(),
+                "com.vaca.chatmygirl.fileProvider",
+                fileJA
+            )
             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
             intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
             requireActivity().startActivityForResult(intent, 96)
@@ -70,26 +69,33 @@ class SendOptionFragment : Fragment() {
         binding.photo.setOnClickListener {
             ImageSelector.show(
                 requireActivity(),
-              MainActivity.REQUEST_CODE_SELECT_IMG,
-               MAX_SELECT_COUNT
+                MainActivity.REQUEST_CODE_SELECT_IMG,
+                MAX_SELECT_COUNT
             )
         }
 
         binding.file.setOnClickListener {
-
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "*/*"
+            this.startActivityForResult(
+                intent,
+                92
+            )
         }
-        fun viewVideo() {
-           val builder_video = AlertDialog.Builder(requireContext()) //创建对话框
 
-            val layout_video =layoutInflater.inflate(R.layout.dialog_select_video, null) //获取自定义布局
+
+        binding.video.setOnClickListener {
+            val builder_video = AlertDialog.Builder(requireContext()) //创建对话框
+            val layout_video = layoutInflater.inflate(R.layout.dialog_select_video, null) //获取自定义布局
             builder_video.setView(layout_video) //设置对话框的布局
-           val dialog_video = builder_video.create() //生成最终的对话框
+            val dialog_video = builder_video.create() //生成最终的对话框
             dialog_video.show() //显示对话框
-           val takeVideoTV = layout_video.findViewById<TextView>(R.id.videoGraph)
-           val chooseVideoTV = layout_video.findViewById<TextView>(R.id.video)
-           val cancelVideoTV = layout_video.findViewById<TextView>(R.id.cancel_video)
+            val takeVideoTV = layout_video.findViewById<TextView>(R.id.videoGraph)
+            val chooseVideoTV = layout_video.findViewById<TextView>(R.id.video)
+            val cancelVideoTV = layout_video.findViewById<TextView>(R.id.cancel_video)
             //设置监听
-            takeVideoTV.setOnClickListener{
+            takeVideoTV.setOnClickListener {
                 dialog_video.dismiss()
                 val intentx = Intent(MediaStore.ACTION_VIDEO_CAPTURE) // 创建一个请求视频的意图
                 intentx.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1) // 设置视频的质量，值为0-1，
@@ -99,20 +105,15 @@ class SendOptionFragment : Fragment() {
                     intentx,
                     93
                 )
-
             }
             chooseVideoTV.setOnClickListener {
                 dialog_video.dismiss()
                 val intent: Intent = Intent(requireActivity(), VideoListActivity::class.java)
                 requireActivity().startActivityForResult(intent, 86)
             }
-            cancelVideoTV.setOnClickListener{
+            cancelVideoTV.setOnClickListener {
                 dialog_video.dismiss()
             }
-        }
-
-        binding.video.setOnClickListener {
-            viewVideo()
         }
 
 
